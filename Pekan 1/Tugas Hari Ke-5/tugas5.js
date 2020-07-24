bf6f29d4f4448d072c9d25132919589aa716e6a7
Hendi Irawan <irawan.dbns@gmail.com>
2020-07-24 11:02:58 +0700

    Tugas Hari Ke5

---
+++ Pekan 1/Tugas Hari Ke-5/tugas5.js
@@ -0,0 +1,29 @@
+//soalno1
+function halo() {
+    console.log("Halo Sanbers!");
+}
+//jawabno1
+halo(); 
+
+//soalno2
+function kalikan(num1,num2){
+  return num1 * num2
+}
+var num1 = 12
+var num2 = 4
+//jawabanno2
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali)
+
+//soalno3
+function buatKalimat(nama, umur, alamat, hobi){
+    return 'Nama Saya ' + nama +', ' + 'Umur Saya ' + umur +', ' + 'Alamat Saya ' + alamat +', ' + 'Hobi Saya ' + hobi;
+}
+
+var nama = "John"
+var umur = 30
+var alamat = "Jalan belum jadi"
+var hobi = "Gaming"
+//jawabanno3
+var hasilKalimat = buatKalimat(nama, umur, alamat, hobi)
+console.log(hasilKalimat)
